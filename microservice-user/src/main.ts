bee43e57e4a7edb72bcb0e19a5c650bef3792389
furbox <furbox@gmail.com>
2021-08-17 10:23:50 -0500

    user microservice

--- microservice-user/src/main.ts
+++ microservice-user/src/main.ts
@@ -5,7 +5,7 @@ import { RabbitMQ } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
-    Transport: Transport.RMQ,
+    transport: Transport.RMQ,
     options: {
       urls: process.env.AMQP_URL,
       queue: RabbitMQ.UserQueue,
